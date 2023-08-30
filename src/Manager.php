a4c9168ddad63a0329a3af61ef6cc931ee3ae312
PartumGames <jemerson6989@gmail.com>
2023-08-30 17:08:34 -0500

    Update Manager.php
    now throws an exception when the user tries to fire an event that doesn't exist

--- src/Manager.php
+++ src/Manager.php
@@ -2,6 +2,7 @@
 
 namespace Jemer\EventDispatcher;
 
+use Exception;
 
 class Manager
 {
@@ -30,6 +31,12 @@ class Manager
     }
     public function FireEvent(string $event)
     {
+        if(!array_key_exists($event, $this->events))
+        {
+            throw new Exception("Could not find event: " . $event);
+            return;
+        }
+
         foreach ($this->events[$event] as $event) 
         {
             if(is_string($event))
